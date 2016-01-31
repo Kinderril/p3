af7c881db7b3c7f6e8cb5c4e7b6869fe9202e107
Kinderril <sarevok007@yandex.ru>
2016-01-31 14:00:18 +0400

    Unit fixes

--- Provinence3/Assets/Script/Unit/Unit.cs
+++ Provinence3/Assets/Script/Unit/Unit.cs
@@ -94,6 +94,7 @@ public class Unit : MonoBehaviour
         {
             inventoryWeapon.Init(this, null);
             inventoryWeapon.gameObject.SetActive(false);
+            inventoryWeapon.transform.SetParent(weaponsContainer);
         }
         Control.SetSpped(Parameters.Parameters[ParamType.Speed]);
         if (InventoryWeapons.Count == 0)
