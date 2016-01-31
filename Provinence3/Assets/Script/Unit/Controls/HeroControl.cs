bfc5007d26818299ac0920385b724ed162fd9da5
Kinderril <sarevok007@yandex.ru>
2016-01-31 14:43:27 +0400

    try to fix hero

--- Provinence3/Assets/Script/Unit/Controls/HeroControl.cs
+++ Provinence3/Assets/Script/Unit/Controls/HeroControl.cs
@@ -162,6 +162,7 @@ public class HeroControl : BaseControl
         {
             SetToDirection(dir);
         }
+        Debug.Log("loookkkk " + dir);
         var doRotate = SpinTransform.SetLookDir(dir);
         return doRotate;
     }
